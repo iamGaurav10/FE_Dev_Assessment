b84aa37a0e4bc861d33901f5210820787735d832
Gaurav <gauravbhangre10@gmail.com>
2024-07-01 09:52:40 +0530

    changes102

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,55 +11,54 @@ import { IoMdStopwatch } from "react-icons/io";
 import { PiTrainFill } from "react-icons/pi";
 
 const Footer = () => {
-  
   return (
     <>
-    <footer className="footer_container">
-      <div className="F_elem_1">
-        <span className="F_heading_elem">Contact Us</span>
-        <p className="F_subheading_elem">
-          Address: amet, consetetur sadipscing elitr, sed diam Email id: eirmod
-          tempor invidunt ut labore et dolore Phone no: 123456789
-        </p>
-      </div>
-      <div className="F_elem_2">
-        <span className="F_heading_elem">Follow Us</span>
-        <div className="logos">
-          <FaFacebookF color="white"  size={20} />
-          <FaLinkedin color="white" size={20} />
-          <FaInstagram color="white" size={20} />
-          <FaTwitter color="white" size={20} />
-          <FaYoutube color="white" size={20} />
-          <FaQuora color="white" size={20} />
+      <footer className="footer_container">
+        <div className="F_elem_1">
+          <span className="F_heading_elem">Contact Us</span>
+          <p className="F_subheading_elem">
+            Address: amet, consetetur sadipscing elitr, sed diam Email id:
+            eirmod tempor invidunt ut labore et dolore Phone no: 123456789
+          </p>
         </div>
-      </div>
-      <div className="F_elem_3">
-        <span className="F_heading_elem">Head Office</span>
-        <span className="F_subheading_elem">
-          <ul>
-            <li>
-              <IoLocationSharp  size={20}/> 
-              Lorem ipsum dolor sit amet consectetur.
-            </li>
-            <li>
-              <IoMdStopwatch size={20}/>
-              Lorem ipsum dolor sit amet consectetur.
-            </li>
-            <li>
-              <IoMdStopwatch size={20}/>
-              Lorem ipsum dolor sit amet consectetur.
-            </li>
-            <li>
-              <PiTrainFill size={20}/>
-              Lorem ipsum dolor sit amet consectetur.
-            </li>
-          </ul>
-        </span>
-      </div>
-    </footer>
-    <div className="the_end">
+        <div className="F_elem_2">
+          <span className="F_heading_elem">Follow Us</span>
+          <div className="logos">
+            <FaFacebookF color="white" className="F_logos" />
+            <FaLinkedin color="white" className="F_logos" />
+            <FaInstagram color="white" className="F_logos" />
+            <FaTwitter color="white" className="F_logos" />
+            <FaYoutube color="white" className="F_logos" />
+            <FaQuora color="white" className="F_logos" />
+          </div>
+        </div>
+        <div className="F_elem_3">
+          <span className="F_heading_elem">Head Office</span>
+          <span className="F_subheading_elem">
+            <ul>
+              <li>
+                <IoLocationSharp size={20} />
+                Lorem ipsum dolor sit amet consectetur.
+              </li>
+              <li>
+                <IoMdStopwatch size={20} />
+                Lorem ipsum dolor sit amet consectetur.
+              </li>
+              <li>
+                <IoMdStopwatch size={20} />
+                Lorem ipsum dolor sit amet consectetur.
+              </li>
+              <li>
+                <PiTrainFill size={20} />
+                Lorem ipsum dolor sit amet consectetur.
+              </li>
+            </ul>
+          </span>
+        </div>
+      </footer>
+      <div className="the_end">
         <p> © 2021 All Rights Reserved. Privacy Policy </p>
-    </div>
+      </div>
     </>
   );
 };
